86623b989428519e3cea85f8301c722e4fa64c85
Eder Rengifo <ederrengifo@Air-de-Eder-3.lan>
2016-11-25 20:14:17 -0500

    tweaks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,10 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to "sessions#new"
+    redirect_to root_url
   end
+
+  def new
+  end
+
 end
